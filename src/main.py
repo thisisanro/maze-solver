fa745de6a5d99e2351a10961d1f736f786e728d5
thisisanro <antonromanenko@tutanota.com>
2025-03-06 15:57:55 +0000

    Add basic methods for the Window class and launching a window from main

--- src/main.py
+++ src/main.py
@@ -1,5 +1,9 @@
+from window import Window
+
+
 def main():
-    print("Hello")
+    win = Window(800, 600)
+    win.wait_for_close()
 
 if __name__ == "__main__":
     main()
--- src/window.py
+++ src/window.py
@@ -5,8 +5,24 @@ class Window():
     def __init__(self, width, height):
         self.width = width
         self.height = height
-        self.root = Tk()
-        self.root.title("Maze solver")
-        self.canvas = Canvas(self.root, width=width, height=height)
-        self.canvas.pack()
-        self.running = False
+        self.__root = Tk()
+        self.__root.title("Maze solver")
+        self.__root.protocol("WM_DELETE_WINDOW", self.close())
+        self.__canvas = Canvas(self.__root, width=width, height=height)
+        self.__canvas.pack()
+        self.__running = False
+
+    def redraw(self):
+        self.__root.update_idletasks()
+        self.__root.update()
+    
+    def wait_for_close(self):
+        self.__running = True
+        while self.__running:
+            self.redraw()
+        print("Window closed")
+
+    def close(self):
+        self.__running = False
+    
+
