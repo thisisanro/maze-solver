d480187d947df3baac13769f48252b2047e3d9af
thisisanro <antonromanenko@tutanota.com>
2025-03-14 15:17:29 +0000

    Add unittest

--- src/main.py
+++ src/main.py
@@ -5,10 +5,10 @@ from maze import Maze
 def main():
     win = Window(800, 600)
 
-    size_x = 50
-    size_y = 50
-    num_rows = 8
-    num_cols = 12
+    size_x = 23
+    size_y = 23
+    num_rows = 10
+    num_cols = 10
     maze = Maze(2, 2, num_rows, num_cols, size_x, size_y, win)
 
     win.wait_for_close()
